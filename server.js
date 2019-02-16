380ce2f0f101bd2b809757fde4ea0420d380705c
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-02-16 15:33:22 -0800

    added template engine

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+app.engine('.handlebars', exphbs({
+    extname: '.handlebars',                  // Set the file extension
+    defaultLayout: 'main',            // Set a default template
+}))
+
 app.get('/', (req, res) => 
 res.send('Hello World!')
 )
@@ -9,10 +15,7 @@ res.send('Hello World!')
 app.listen(port, () => 
 console.log(`Example app listening on port ${port}!`))
 
-// app.engine('.hbs', exphbs({
-//     extname: '.hbs',                  // Set the file extension
-//     defaultLayout: 'main',            // Set a default template
-// }))
+
 
 // mongoose.Promise = global.Promise;
 // mongoose.connect("mongodb://localhost/redditclone", { useMongoClient: true });
