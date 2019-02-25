5a4bb73fc60b9679a666c95b11e5c9cca80c2df8
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-02-24 20:33:47 -0800

    fixed post tests

--- test/posts.js
+++ test/posts.js
@@ -2,10 +2,10 @@ const app = require("./../server");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
-
-
 const Post = require('../models/post');
+const User = require("../models/user");
 const server = require('../server');
+const agent = chai.request.agent(app);
 
 chai.should();
 chai.use(chaiHttp);
@@ -18,6 +18,25 @@ describe('Posts', function () {
     url: 'https://www.google.com',
     summary: 'post summary',
   };
+
+  const user = {
+    username: "poststest",
+    password: "testposts"
+  };
+
+  before(function(done) {
+    agent
+      .post("/sign-up")
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send(user)
+      .then(function(res) {
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  });
+
   it('Should create with valid attributes at POST /posts/new', function (done) {
     // Checks how many posts there are now
     Post.estimatedDocumentCount()
@@ -51,7 +70,24 @@ describe('Posts', function () {
         done(err);
       });
   });
-  after(function () {
-    Post.findOneAndDelete(newPost);
+  after(function (done) {
+    Post.findOneAndDelete(newPost)
+      .then(function (res) {
+        agent.close()
+
+        User.findOneAndDelete({
+          username: user.username
+        })
+          .then(function (res) {
+            done()
+          })
+          .catch(function (err) {
+            done(err);
+          });
+      })
+      .catch(function (err) {
+        done(err);
+      });
   });
-    });
+
+});
