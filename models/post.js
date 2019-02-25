b9887904da60f96e1905e5d427b0cee321b60eba
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-02-24 20:39:14 -0800

    implemented remaining auth tests. passing all tests except post

--- models/post.js
+++ models/post.js
@@ -1,6 +1,7 @@
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Populate = require("../utils/autopopulate");
 
 const PostSchema = new Schema({
     author: { type: Schema.Types.ObjectId, ref: "User", required: true },
