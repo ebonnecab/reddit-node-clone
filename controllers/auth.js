b82c962f17fdf639f3175ebbebdef864b5cbe435
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-02-24 18:11:18 -0800

    authors for posts are visible

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,14 +2,31 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 module.exports = app => {
-
+  //sign up
   app.get("/sign-up", (req, res) => {
     res.render("sign-up");
   });
-
+  //login
   app.get('/login', (req, res) => {
     res.render('login');
   });
+
+  app.post("/sign-up", (req, res) => {
+
+    // Create User
+    const user = new User(req.body);
+
+    user.save().then((user) => {
+      var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+      res.redirect('/');
+    })
+      .catch(err => {
+        console.log(err.message);
+        return res.status(400).send({ err: err });
+      });
+  });
+
 
   app.post("/login", (req, res) => {
     const username = req.body.username;
@@ -46,25 +63,6 @@ module.exports = app => {
       })
       .catch(err => {
         console.log(err);
-      });
-  });
-
-  app.post("/sign-up", (req, res) => {
-    const user = new User(req.body);
-    user
-      .save()
-      .then(user => {
-
-        let token = jwt.sign({ _id: user._id },
-          process.env.SECRET, { expiresIn: "60 days" });
-
-        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-
-        res.redirect("/");
-      })
-      .catch(err => {
-        console.log(err);
-        return res.status(400).send({ err: err.message });
       });
   });
 
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,39 +1,54 @@
+const User = require("../models/user");
 const Post = require('../models/post');
 
 module.exports = (app) => {
 
     app.get('/', (req, res) => {
         const currentUser = req.user;
-        Post.find({}).then(posts => {
-            res.render("posts-index", { posts, currentUser });
-        }).catch(err => {
-            console.log(err.message)
-        })
+        console.log(req.cookies);
+        Post.find().populate('author')
+            .then(posts => {
+                res.render('posts-index', { posts, currentUser });
+            }).catch(err => {
+                console.log(err.message);
+            })
     })
 
     app.get('/posts/new', (req, res) => {
-        const currentUser = req.user;
         res.render('posts-new', { currentUser })
     })
 
     // CREATE
     app.post("/posts/new", (req, res) => {
-        if (req.user) {
-            var post = new Post(req.body);
+    if (req.user) {
+      const post = new Post(req.body);
+      post.author = req.user._id;
 
-            post.save(function (err, post) {
-                return res.redirect(`/`);
-            });
-        } else {
-            return res.status(401); // UNAUTHORIZED
-        }
-    });
+      post
+        .save()
+        .then(post => {
+          return User.findById(req.user._id);
+        })
+        .then(user => {
+          user.posts.unshift(post);
+          user.save();
+
+          res.redirect(`/posts/${post._id}`);
+        })
+        .catch(err => {
+          console.log(err.message);
+        });
+    } else {
+      return res.status(401);
+    }
+  });
+
 
 
     app.get("/posts/:id", function (req, res) {
         const currentUser = req.user;
         // LOOK UP THE POST
-        Post.findById(req.params.id)
+        Post.findById(req.params.id).populate('comments').populate('author')
           .populate("comments")
           .then(post => {
             res.render("posts-show", { post, currentUser });
@@ -46,7 +61,7 @@ module.exports = (app) => {
     // SUBREDDIT
     app.get("/n/:subreddit", function (req, res) {
         const currentUser = req.user;
-        Post.find({ subreddit: req.params.subreddit })
+        Post.find({ subreddit: req.params.subreddit }).populate('author')
             .then(posts => {
                 res.render("posts-index", { posts, currentUser });
             })
